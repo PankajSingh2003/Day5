8ae7fd10638e3a5e7fb2602e182707580b2983c5
PankajSingh2003 <pankajsheelendrasingh@gmail.com>
2022-09-09 03:59:26 +0530

    This are all the files

---
+++ forwithrange.py
@@ -0,0 +1,8 @@
+# range is a function that is used to loop through a list and then 
+for number in range(0,10):
+    print(number)
+total=0
+# add one more comma to add more other than one increment 
+for i in range(1,101):
+    total+=i
+print(total)
